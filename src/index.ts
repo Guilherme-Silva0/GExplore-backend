de21e04c5c37c9c8706aace9a77523a1eb325d3a
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-09-29 18:26:42 -0300

    :sparkles: feat:  Add user authentication functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { AuthenticateUserController } from './adapters/user/AuthenticateUserController'
 import { RegisterUserController } from './adapters/user/RegisterUserController'
+import { AuthenticateUser } from './core/user/service/AuthenticateUser'
 import { RegisterUser } from './core/user/service/RegisterUser'
 import { UserRepositoryPrisma } from './external/prisma/UserRepositoryPrisma'
 import { app } from './external/web/config'
@@ -7,3 +9,6 @@ const userRepositoryPrisma = new UserRepositoryPrisma()
 
 const registerUser = new RegisterUser(userRepositoryPrisma)
 new RegisterUserController(app, registerUser)
+
+const authenticateUser = new AuthenticateUser(userRepositoryPrisma)
+new AuthenticateUserController(app, authenticateUser)
